c4c48efbd6e6ddba64761b36bc56b42f365234c0
Rohit Girdhar <rohitgird12@gmail.com>
2016-12-20 21:55:46 +0530

    New Commit

--- Assets/Scripts/index.js
+++ Assets/Scripts/index.js
@@ -27,16 +27,61 @@ document.getElementById('btn').addEventListener('click', () => {
 });
 
 
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: [],
+        scrollingSpeed: 110,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3', 'section4']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
 
-$('#pagepiling').pagepiling({
-    onLeave: function(index, nextIndex, direction){
-        
-        if(index == 1 && direction =='down'){
-            console.log("Going to section 3!");
-        }
+        //events
+        onLeave: function(index, nextIndex, direction){
 
-        else if(index == 2 && direction == 'up'){
-            console.log("Going to section 1!");
+        },
+        afterLoad: function(anchorLink, index){
+
+
+            if(index == 2){
+                var ChNum = $('#getOne');
+                $(ChNum).addClass('ChNum');
+                var ChNum2 = $('#setTwo');
+                $(ChNum2).addClass('ChNum2');
+            }
+
+            else if(index == 3){
+                var ChNum = $('#getTwo');
+                $(ChNum).addClass('ChNum');
+                var ChNum2 = $('#setThree');
+                $(ChNum2).addClass('ChNum2');
+            }
+            else if(index == 4){
+                var ChNum = $('#getThree');
+                $(ChNum).addClass('ChNum');
+                var ChNum2 = $('#setFour');
+                $(ChNum2).addClass('ChNum2');
+            }
+        },
+
+        afterRender : function() {
+            $('#one').toggleClass('Zoom-In');
         }
-    }
+    });
 });
